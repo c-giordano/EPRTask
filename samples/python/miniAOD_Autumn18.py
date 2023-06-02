a185a27b3a29bb71674bd6bbc7cd8fd42bb2dea4
cristina.giordano <cristina.giordano@oeaw.ac.at>
2023-06-02 15:31:46 +0200

    move to MINI and add DY samples

---
+++ samples/python/miniAOD_Autumn18.py
@@ -0,0 +1,56 @@
+import copy, os, sys
+from RootTools.core.standard import *
+import ROOT
+
+def get_parser():
+    import argparse
+    argParser = argparse.ArgumentParser(description = "Argument parser for samples file")
+    argParser.add_argument('--logLevel',    action='store', nargs='?',  choices=['CRITICAL', 'ERROR', 'WARNING', 'INFO', 'DEBUG', 'TRACE', 'NOTSET'],  default='INFO', help="Log level for logging")
+    argParser.add_argument('--overwrite',   action='store_true',    help="Overwrite current entry in db?")
+    argParser.add_argument('--update',      action='store_true',    help="Update current entry in db?")
+    argParser.add_argument('--check_completeness', action='store_true',    help="Check competeness?")
+    return argParser
+
+# Logging
+if __name__=="__main__":
+    args = get_parser().parse_args()
+    ov = args.overwrite
+    if args.update:
+        ov = 'update'
+    import Samples.Tools.logger as logger
+    logger = logger.get_logger(args.logLevel, logFile = None )
+    import RootTools.core.logger as logger_rt
+    logger_rt = logger_rt.get_logger(args.logLevel, logFile = None )
+else:
+    import logging
+    logger = logging.getLogger(__name__)
+    ov = False
+
+from Samples.Tools.config import  redirector_global
+redirector = redirector_global
+
+## DB
+from Samples.Tools.config import dbDir
+dbFile = dbDir+'/JetTracking_AOD.sql'
+logger.info("Using db file: %s", dbFile)
+
+DYJetsToLL_M50_NLO    = FWLiteSample.fromDAS("DYJetsToLL_M50_NLO", "/DYJetsToLL_M-50_TuneCP5_13TeV-amcatnloFXFX-pythia8/RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15-v1/MINIAODSIM", dbFile=dbFile, overwrite=ov, prefix=redirector_global, skipCheck=True)
+DYJetsToLL_M50_LO     = FWLiteSample.fromDAS("DYJetsToLL_M50_LO", "/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15-v1/MINIAODSIM", dbFile=dbFile, overwrite=ov, prefix=redirector_global, skipCheck=True)
+
+DYJetsToLL_M10to50_LO = FWLiteSample.fromDAS("DYJetsToLL_M10to50_LO", "/DYJetsToLL_M-10to50_TuneCP5_13TeV-madgraphMLM-pythia8/RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15-v2/MINIAODSIM", dbFile=dbFile, overwrite=ov, prefix=redirector_global, skipCheck=True)
+
+DYJetsToLL_M4to50_HT70to100_LO      = FWLiteSample.fromDAS("DYJetsToLL_M4to50_HT70to100_LO",      "/DYJetsToLL_M-4to50_HT-70to100_TuneCP5_PSweights_13TeV-madgraphMLM-pythia8/RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15-v1/MINIAODSIM", dbFile=dbFile, overwrite=ov, prefix=redirector_global, skipCheck=True) # https://cms-pdmv.cern.ch/pmp/historical?r=HIG-RunIIAutumn18MiniAOD-01083
+DYJetsToLL_M4to50_HT100to200_LO     = FWLiteSample.fromDAS("DYJetsToLL_M4to50_HT100to200_LO",     "/DYJetsToLL_M-4to50_HT-100to200_TuneCP5_PSweights_13TeV-madgraphMLM-pythia8/RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15-v1/MINIAODSIM", dbFile=dbFile, overwrite=ov, prefix=redirector_global, skipCheck=True)
+DYJetsToLL_M4to50_HT200to400_LO     = FWLiteSample.fromDAS("DYJetsToLL_M4to50_HT200to400_LO",     "/DYJetsToLL_M-4to50_HT-200to400_TuneCP5_PSweights_13TeV-madgraphMLM-pythia8/RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15-v1/MINIAODSIM", dbFile=dbFile, overwrite=ov, prefix=redirector_global, skipCheck=True)
+DYJetsToLL_M4to50_HT400to600_LO     = FWLiteSample.fromDAS("DYJetsToLL_M4to50_HT400to600_LO",     "/DYJetsToLL_M-4to50_HT-400to600_TuneCP5_PSweights_13TeV-madgraphMLM-pythia8/RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15-v1/MINIAODSIM", dbFile=dbFile, overwrite=ov, prefix=redirector_global, skipCheck=True)
+DYJetsToLL_M4to50_HT600toInf_LO     = FWLiteSample.fromDAS("DYJetsToLL_M4to50_HT600toInf_LO",     "/DYJetsToLL_M-4to50_HT-600toInf_TuneCP5_PSWeights_13TeV-madgraphMLM-pythia8/RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15-v1/MINIAODSIM", dbFile=dbFile, overwrite=ov, prefix=redirector_global, skipCheck=True)
+
+DYJetsToLL_M50_HT70to100_LO         = FWLiteSample.fromDAS("DYJetsToLL_M50_HT70to100_LO",         "/DYJetsToLL_M-50_HT-70to100_TuneCP5_PSweights_13TeV-madgraphMLM-pythia8/RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15-v1/MINIAODSIM", dbFile=dbFile, overwrite=ov, prefix=redirector_global, skipCheck=True)
+DYJetsToLL_M50_HT100to200_LO        = FWLiteSample.fromDAS("DYJetsToLL_M50_HT100to200_LO",        "/DYJetsToLL_M-50_HT-100to200_TuneCP5_PSweights_13TeV-madgraphMLM-pythia8/RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15-v2/MINIAODSIM", dbFile=dbFile, overwrite=ov, prefix=redirector_global, skipCheck=True)
+DYJetsToLL_M50_HT200to400_LO        = FWLiteSample.fromDAS("DYJetsToLL_M50_HT200to400_LO",        "/DYJetsToLL_M-50_HT-200to400_TuneCP5_PSweights_13TeV-madgraphMLM-pythia8/RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15-v2/MINIAODSIM", dbFile=dbFile, overwrite=ov, prefix=redirector_global, skipCheck=True)
+DYJetsToLL_M50_HT400to600_LO        = FWLiteSample.fromDAS("DYJetsToLL_M50_HT400to600_LO",        "/DYJetsToLL_M-50_HT-400to600_TuneCP5_PSweights_13TeV-madgraphMLM-pythia8/RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15-v7/MINIAODSIM", dbFile=dbFile, overwrite=ov, prefix=redirector_global, skipCheck=True)
+DYJetsToLL_M50_HT400to600_LO_ext2   = FWLiteSample.fromDAS("DYJetsToLL_M50_HT400to600_LO_ext2",   "/DYJetsToLL_M-50_HT-400to600_TuneCP5_PSweights_13TeV-madgraphMLM-pythia8/RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15_ext2-v3/MINIAODSIM", dbFile=dbFile, overwrite=ov, prefix=redirector_global, skipCheck=True)
+DYJetsToLL_M50_HT600to800_LO        = FWLiteSample.fromDAS("DYJetsToLL_M50_HT600to800_LO",        "/DYJetsToLL_M-50_HT-600to800_TuneCP5_PSweights_13TeV-madgraphMLM-pythia8/RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15-v2/MINIAODSIM", dbFile=dbFile, overwrite=ov, prefix=redirector_global, skipCheck=True)
+DYJetsToLL_M50_HT800to1200_LO       = FWLiteSample.fromDAS("DYJetsToLL_M50_HT800to1200_LO",       "/DYJetsToLL_M-50_HT-800to1200_TuneCP5_PSweights_13TeV-madgraphMLM-pythia8/RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15-v2/MINIAODSIM", dbFile=dbFile, overwrite=ov, prefix=redirector_global, skipCheck=True)
+DYJetsToLL_M50_HT1200to2500_LO      = FWLiteSample.fromDAS("DYJetsToLL_M50_HT1200to2500_LO",      "/DYJetsToLL_M-50_HT-1200to2500_TuneCP5_PSweights_13TeV-madgraphMLM-pythia8/RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15-v2/MINIAODSIM", dbFile=dbFile, overwrite=ov, prefix=redirector_global, skipCheck=True)
+DYJetsToLL_M50_HT2500toInf_LO       = FWLiteSample.fromDAS("DYJetsToLL_M50_HT2500toInf_LO",       "/DYJetsToLL_M-50_HT-2500toInf_TuneCP5_PSweights_13TeV-madgraphMLM-pythia8/RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15-v2/MINIAODSIM", dbFile=dbFile, overwrite=ov, prefix=redirector_global, skipCheck=True)

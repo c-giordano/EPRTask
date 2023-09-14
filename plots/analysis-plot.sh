16532310e6dad9448a1a81ab77f3ad7655ac9b86
cristina.giordano <cristina.giordano@oeaw.ac.at>
2023-09-14 11:20:11 +0200

    new

--- plots/analysis-plot.sh
+++ plots/analysis-plot.sh
@@ -2,23 +2,36 @@
 # python analysis-plot.py --version 2 --eta_cut coarse --simple_cut pt
 # python analysis-plot.py --version 2 --eta_cut coarse --simple_cut mass
 # python analysis-plot.py --version 2 --eta_cut coarse --simple_cut r
-python analysis-plot.py --version 2 --eta_cut coarse --simple_cut jetPt
+# python analysis-plot.py --version 2.1 --eta_cut coarse --simple_cut jetPt
+python analysis-plot.py --version 2.2 --eta_cut coarse --simple_cut jetPt_high
+python analysis-plot.py --version 2.2 --eta_cut coarse --simple_cut r_high
 # Coarse composite
 # python analysis-plot.py --version 2 --eta_cut coarse --simple_cut pt --composite_cut mass
 # python analysis-plot.py --version 2 --eta_cut coarse --simple_cut pt --composite_cut r
 # python analysis-plot.py --version 2 --eta_cut coarse --simple_cut mass --composite_cut r
-python analysis-plot.py --version 2 --eta_cut coarse --simple_cut pt --composite_cut jetPt
-python analysis-plot.py --version 2 --eta_cut coarse --simple_cut mass --composite_cut jetPt
-python analysis-plot.py --version 2 --eta_cut coarse --simple_cut r --composite_cut jetPt
+# python analysis-plot.py --version 2.1 --eta_cut coarse --simple_cut pt --composite_cut jetPt
+# python analysis-plot.py --version 2.1 --eta_cut coarse --simple_cut mass --composite_cut jetPt
+# python analysis-plot.py --version 2.1 --eta_cut coarse --simple_cut r --composite_cut jetPt
+python analysis-plot.py --version 2.2 --eta_cut coarse --simple_cut pt --composite_cut jetPt_high
+python analysis-plot.py --version 2.2 --eta_cut coarse --simple_cut mass --composite_cut jetPt_high
+python analysis-plot.py --version 2.2 --eta_cut coarse --simple_cut r_high --composite_cut jetPt_high
+
 #Fine simple
 # python analysis-plot.py --version 2 --eta_cut fine --simple_cut pt
 # python analysis-plot.py --version 2 --eta_cut fine --simple_cut mass
 # python analysis-plot.py --version 2 --eta_cut fine --simple_cut r
-python analysis-plot.py --version 2 --eta_cut fine --simple_cut jetPt
+# python analysis-plot.py --version 2.1 --eta_cut fine --simple_cut jetPt
+python analysis-plot.py --version 2.2 --eta_cut fine --simple_cut jetPt_high
+python analysis-plot.py --version 2.2 --eta_cut fine --simple_cut r_high
 #Fine composite
 # python analysis-plot.py --version 2 --eta_cut fine --simple_cut pt --composite_cut mass
 # python analysis-plot.py --version 2 --eta_cut fine --simple_cut pt --composite_cut r
 # python analysis-plot.py --version 2 --eta_cut fine --simple_cut mass --composite_cut r
-python analysis-plot.py --version 2 --eta_cut fine --simple_cut pt --composite_cut jetPt
-python analysis-plot.py --version 2 --eta_cut fine --simple_cut mass --composite_cut jetPt
-python analysis-plot.py --version 2 --eta_cut fine --simple_cut r --composite_cut jetPt
+# python analysis-plot.py --version 2.1 --eta_cut fine --simple_cut pt --composite_cut jetPt
+# python analysis-plot.py --version 2.1 --eta_cut fine --simple_cut mass --composite_cut jetPt
+# python analysis-plot.py --version 2.1 --eta_cut fine --simple_cut r --composite_cut jetPt
+python analysis-plot.py --version 2.2 --eta_cut fine --simple_cut pt --composite_cut jetPt_high
+python analysis-plot.py --version 2.2 --eta_cut fine --simple_cut mass --composite_cut jetPt_high
+python analysis-plot.py --version 2.2 --eta_cut fine --simple_cut r_high --composite_cut jetPt_high
+#Scatter plot
+# python analysis-plot.py --version 2 --plots Scatter
